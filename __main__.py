af675601fd928d9f591cc0bdc2d487f60859bebc
Ethan Meanor <df>
2021-11-26 11:53:07 -0500

    Fix and refactor

--- __main__.py
+++ __main__.py
@@ -6,21 +6,11 @@ def get_notebooks(notebook_numbers, outline_only=True):
 
     notebooks = []
     for number in notebook_numbers:
-        notebook = scraper.scrape_notebook(1887, number)
+        blocks = scraper.scrape_notebook(1887, number)
 
-        blocks = []
-        for block in notebook:
-            blocks.append(block)
-
-        notebooks.append({
-            'notebook_number': number,
-            'blocks': blocks 
-        })
-
-    texts = []
-    for notebook in notebooks:
-        notebook_texts = []
-        for block in notebook['blocks']:
+        print('Processing texts...')
+        texts = []
+        for block in blocks:
             nietzsche_num = parse.nietzsche_number(block)
 
             if outline_only == True and nietzsche_num is None:
@@ -29,15 +19,18 @@ def get_notebooks(notebook_numbers, outline_only=True):
             kgw_nums = parse.kgw_numbers(block)
             full_text = parse.text(block)
 
-            notebook_texts.append({
+            texts.append({
                 'nietzsche_number': nietzsche_num,
                 'kgw_notebook_number': kgw_nums[0],
                 'kgw_text_number': kgw_nums[1],
                 'text': full_text
             })
-        
-        texts.append(notebook_texts)
 
-    print(texts)
+        notebooks.append({
+            'notebook_number': number,
+            'texts': texts
+        })
 
-get_notebooks([9])
+    print(notebooks)
+
+get_notebooks([9, 11])
