afcc214606a44bbf80a23b114ca51cb8e6f3b5e8
Ethan Meanor <df>
2021-11-25 19:54:40 -0500

    Working

--- __main__.py
+++ __main__.py
@@ -1,29 +1,41 @@
 from scraper import NietzscheScraper
 import parse
 
-def export_374():
+def export(notebook_numbers):
     scraper = NietzscheScraper()
 
-    # blocks = []
-    # for n in range (9, 12):
-    #     for block in scraper.scrape_notebook(1887, n):
-    #         blocks.append(block)
+    notebooks = []
+    for number in notebook_numbers:
+        notebook = scraper.scrape_notebook(1887, number)
+
+        blocks = []
+        for block in notebook:
+            blocks.append(block)
+
+        notebooks.append({
+            'notebook_number': number,
+            'blocks': blocks 
+        })
 
     texts = []
-    for block in scraper.scrape_notebook(1887, 9):
-        nietzsche_num = parse.nietzsche_number(block)
+    for notebook in notebooks:
+        notebook_texts = []
+        for block in notebook['blocks']:
+            nietzsche_num = parse.nietzsche_number(block)
 
-        if nietzsche_num is not None:
-            kgw_nums = parse.kgw_numbers(block)
-            full_text = parse.text(block)
+            if nietzsche_num is not None:
+                kgw_nums = parse.kgw_numbers(block)
+                full_text = parse.text(block)
 
-            texts.append({
-                'nietzsche_number': nietzsche_num,
-                'kgw_notebook_number': kgw_nums[0],
-                'kgw_text_number': kgw_nums[1],
-                'text': full_text
-            })
+                notebook_texts.append({
+                    'nietzsche_number': nietzsche_num,
+                    'kgw_notebook_number': kgw_nums[0],
+                    'kgw_text_number': kgw_nums[1],
+                    'text': full_text
+                })
+        
+        texts.append(notebook_texts)
 
     print(texts)
 
-export_374()
+export_374([9])
